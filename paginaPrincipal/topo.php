30892075bb3f705fcf2839052269f50753b6048a
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-11-17 14:48:41 -0300

    Primeiro Passos para o topo do site.

--- paginaPrincipal/topo.php
+++ paginaPrincipal/topo.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pizzaria Guerrier</title>
+    <link rel="stylesheet" href="styleDoTopo.css">
 </head>
 <body>
-    
+    <header>
+         <img src="LogoPizzaria.jpeg" alt="Logo da Pizzaria Guerrier">
+
+         <nav>
+            <a href=""></a>
+
+
+         </nav>
+
+
+    </header>
 </body>
 </html>
