50bded6d68b3ac48e39d521bb4f5f43d2e489d7e
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-11-17 15:45:48 -0300

    alinhamento da imagem

--- paginaPrincipal/topo.php
+++ paginaPrincipal/topo.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <header>
-         <img src="Logo da Pizzaria Guerrier" alt="LogoPizzaria.jpeg">
+         <img src="LogoPizzaria.jpeg" alt="Logo Pizzaria Guerrier">
 
          <nav>
             <a href="botão"><strong>Bem-Vindo a Pizzaria Guerrier</strong></a>
