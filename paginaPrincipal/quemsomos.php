cefb22737c39e4929108e9ea65b964d5469a2bbb
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-11-26 19:01:40 -0300

    alterações para ajuste do site principal

--- paginaPrincipal/quemsomos.php
+++ paginaPrincipal/quemsomos.php
@@ -1,12 +1,7 @@
 <?php
 // quemsomos.php
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quem Somos - Pizzaria Bella Napoli</title>
+
     <style>
         body {
             margin: 0;
