e3851ae64377269e6a1741d492556b69030aba2a
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-11-28 18:13:36 -0300

    teste pagina

--- paginaPrincipal/index.php
+++ paginaPrincipal/index.php
@@ -2,5 +2,5 @@
 
 include_once("Topo.php");
 include_once("conteudo.php");
-
+include_once("rodape.php");
 ?>
---
+++ paginaPrincipal/rodape.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="style.css">      
+      </nav>
+        <footer>
+                <img src="imagens/logo_burgues_laranja_2 (1).png" alt="Logo O Burguês">
+                <p></p>
+                &copy; 2025 <strong>O Burguês</strong>. Todos os direitos reservados.
+                <p></p>
+                <hr>
+                Compartilhe algo Delicioso
+                <p></p>
+                <a href="https://www.instagram.com/hamburgueriaoburgues/" target="_blank">
+                 <i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/hamburgueriaoburgues" target="_blank">
+                 <i class="fab fa-facebook"></i></a>
+                <a href="https://www.tiktok.com/@oburgueshamburgueria" target="_blank">
+                 <i class="fab fa-tiktok"></i></a>
+                <h5>Sujeito à disponibilidade em estoque. Imagens meramente ilustrativas.</h5>
+        </footer>
+        
+</body>
+</html>
