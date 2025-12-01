56c4f0eebe8d0981062ca229542b53f061f1b9bd
Giuli0196 <giulieduardox3@gmail.com>
2025-11-30 22:15:14 -0300

    Create buscarEditar.php

---
+++ buscarEditar.php
@@ -0,0 +1,15 @@
+<?php
+include "conexao.php";
+
+$editando = null;
+
+if (isset($_GET['edit'])) {
+
+    $id = $_GET['edit'];
+
+    $sql = "SELECT * FROM hamburguer WHERE id_hamburguer = '$id'";
+    $res = mysqli_query($con, $sql);
+
+    $editando = mysqli_fetch_assoc($res);
+}
+?>
