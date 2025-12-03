d4f058753b919ea7b4399f711518012942391ea8
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-12-03 19:37:07 -0300

    correção erro

--- admindois/excluirdois.php
+++ admindois/excluirdois.php
@@ -1,5 +1,6 @@
 <?php
-include_once("../conexaodois.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/Trabalho_um_php/diretorioPrincipal/restaurante-trabalho-php/conexaodois.php");
+
 
 $id = (int)($_GET['id'] ?? 0);
 
@@ -7,9 +8,9 @@ if ($id > 0) {
 
     $stmt = mysqli_prepare($con, "DELETE FROM clientes WHERE id = ?");
     mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execaaaaaateva($stmt);
+    mysqli_stmt_execute($stmt);
 
 }
 
-header("Location: listardois.php?msg=Cliente excluído!");
+header("Location: listardois.php?msg=Cliente excluído com sucesso!");
 exit;
