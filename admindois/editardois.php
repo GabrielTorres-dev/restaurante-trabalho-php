9858c79c884f23504cb8ac64cce491c602bd7978
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-12-03 19:22:14 -0300

    correção de errros

--- admindois/editardois.php
+++ admindois/editardois.php
@@ -1,5 +1,6 @@
 <?php
-include_once("../conexaodois.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/Trabalho_um_php/diretorioPrincipal/restaurante-trabalho-php/conexaodois.php");
+
 
 $id = (int)($_GET['id'] ?? 0);
 
