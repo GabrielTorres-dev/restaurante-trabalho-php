9e442f2df9f68f288c3aca85a1f696caba727fa1
Giuli0196 <giulieduardox3@gmail.com>
2025-12-03 02:27:30 -0300

    Create listagem.php

---
+++ listagem.php
@@ -0,0 +1,33 @@
+<?php
+include "conexao.php";
+
+$sql = "SELECT * FROM hamburguer";
+$res = mysqli_query($con, $sql);
+?>
+
+<h2>Lista de Hambúrgueres</h2>
+
+<table border="1" cellpadding="8">
+    <tr>
+        <th>ID</th>
+        <th>Tipo</th>
+        <th>Valor</th>
+        <th>Ações</th>
+    </tr>
+
+    <?php while ($h = mysqli_fetch_assoc($res)): ?>
+    <tr>
+        <td><?= $h['id_hamburguer'] ?></td>
+        <td><?= $h['TipodoHamburguer'] ?></td>
+        <td>R$ <?= number_format($h['valorDoHamburguer'], 2, ',', '.') ?></td>
+
+        <td>
+            <a href="?edit=<?= $h['id_hamburguer'] ?>">Editar</a> |
+            <a href="?delete=<?= $h['id_hamburguer'] ?>"
+               onclick="return confirm('Tem certeza que deseja excluir?');">
+               Excluir
+            </a>
+        </td>
+    </tr>
+    <?php endwhile; ?>
+</table>
