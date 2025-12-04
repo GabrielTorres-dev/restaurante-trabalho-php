e1b67d7b35c7f9534350e6e490235948cf836d93
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-12-04 14:55:05 -0300

    correção de erro.

--- admin/configIncUm.php
+++ admin/configIncUm.php
@@ -2,7 +2,7 @@
 
     $conexao = mysqli_connect("localhost","root","");
 
-    $db = mysqli_select_db ($conexao, "cadastrocliente");
+    $db = mysqli_select_db ($conexao, "cadastroclientes");
 
     if($conexao){ 
         echo" <h2>Erro ao conectar ao Banco de dados</h2>";
