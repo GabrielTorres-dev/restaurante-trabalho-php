46461b4e4fa09d3a9e74d00ddc3253227521437b
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-12-04 13:49:21 -0300

    teste

---
+++ admindois/clientesForm.php
@@ -0,0 +1,15 @@
+<h2>Cadastrar Cliente</h2>
+<form action="?pg=clientes-cadastro" method="POST">
+
+    <label>Nome:</label>
+    <input type="text" name="nome_cliente"><br>
+
+    <label>Telefone:</label>
+    <input type="text" name="numero_contato"><br>
+
+    <label>Endereço:</label>
+    <input type="text" name="endereco"><br>
+
+    <input type="submit" value="Cadastrar Cliente">
+
+</form>
