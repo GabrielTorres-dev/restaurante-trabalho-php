fbfee8f8374882cfcbbeb1b6c0bdb391fbcb7501
Paulo Victor <paulov2oficial@gmail.com>
2025-12-04 00:33:09 -0300

    modificação do index

--- admindois/index.php
+++ admindois/index.php
@@ -1,14 +1,20 @@
 <?php
 
-    echo "<h1>Painel administrativo</h1>";
+echo "<h1>Painel administrativo</h1>";
 
-    echo "<a href='?pg=clientes-admin'>Listar Clientes</a> | ";
+echo "<a href='?pg=clientes-admin'>Listar Clientes</a> | ";
+echo "<a href='?pg=clientes-cadastro'>Cadastrar Cliente</a> | ";
 
-    // área de conteúdo
-    if(empty($_SERVER['QUERY_STRING'])){
-       echo "<h3>Bem-vindo ao painel admin.";
-    }else {
-        $pg = "$_GET[pg]";
+
+if (empty($_SERVER['QUERY_STRING'])) {
+    echo "<h3>Bem-vindo ao painel admin.</h3>";
+} else {
+    $pg = $_GET['pg'];
+
+    
+    if (file_exists("$pg.php")) {
         include_once "$pg.php";
+    } else {
+        echo "<h3>Página '$pg' não encontrada.</h3>";
     }
-
+}
