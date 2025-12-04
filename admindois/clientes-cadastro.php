cc1da54cb14028354a36ab9e0a65baa363a49234
Paulo Victor <paulov2oficial@gmail.com>
2025-12-04 01:04:25 -0300

    Correção do cadastro do cliente

--- admindois/clientes-cadastro.php
+++ admindois/clientes-cadastro.php
@@ -1,23 +1,42 @@
 <?php
+require_once "config.inc.php";
 
-    require_once "config.inc.php";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $nome_cliente = $_POST["nome_cliente"];
-        $numero_contato = $_POST["numero_contato"];
-        $endereco = $_POST["endereco"];
+    $nome_cliente     = $_POST["nome_cliente"];
+    $numero_contato   = $_POST["numero_contato"];
+    $endereco         = $_POST["endereco"];
 
-        $sql = "INSERT INTO clientes (nome_cliente, numero_contato, endereco)
-                VALUES ('$nome_cliente', '$numero_contato', '$endereco')";
-        if(mysqli_query($conexao, $sql)){
-            echo "<h3>Cliente cadastrado com sucesso!</h3>";
-            echo "<a href='?pg=clientes-admin'>Voltar</a>";
-        }else{
-            echo "<h3>Erro ao cadastrar cliente!</h3>";
-        }
-    }else{
-        echo "<h2>Acesso negado!</h2>";
+    $sql = "INSERT INTO clientes (nome_cliente, numero_contato, endereco)
+            VALUES ('$nome_cliente', '$numero_contato', '$endereco')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "<h3>Cliente cadastrado com sucesso!</h3>";
         echo "<a href='?pg=clientes-admin'>Voltar</a>";
+    } else {
+        echo "<h3>Erro ao cadastrar cliente!</h3>";
     }
 
-    mysqli_close($conexao);
+} else {
+?>
+
+<h2>Cadastrar Cliente</h2>
+<form action="" method="POST">
+
+    <label>Nome:</label>
+    <input type="text" name="nome_cliente"><br>
+
+    <label>Telefone:</label>
+    <input type="text" name="numero_contato"><br>
+
+    <label>Endereço:</label>
+    <input type="text" name="endereco"><br>
+
+    <input type="submit" value="Cadastrar Cliente">
+
+</form>
+
+<?php
+}
+mysqli_close($conexao);
+?>
